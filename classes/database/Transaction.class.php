cf3548fdd3e1a1e07d90b6637b46d52e8127bfbc
Julien Vaslet <julien.vaslet@gmail.com>
2014-03-25 13:02:18 +0100

    Create Transaction.class.php

---
+++ classes/database/Transaction.class.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace database;
+
+require_once( dirname( __FILE__ )."/Object.class.php" );
+
+final class Bill extends Object
+{
+	protected static $schema = "bank";
+	protected static $table = "transaction";
+	
+	protected static $fields = array(
+		"transaction_id" => array(
+			"type" 			=> "integer",
+			"bits"			=> 24,
+			"unsigned" 		=> true,
+			"autoIncrement"	=> true
+		),
+		"account_id" => array(
+			"type" 			=> "integer",
+			"bits"			=> 24,
+			"unsigned" 		=> true
+		),
+		"transaction_date" => array(
+			"type"		=> "date"
+		),
+		"value_date" => array(
+			"type"		=> "date"
+		),
+		"label" => array(
+			"type"		=> "string",
+			"maxlength"	=> 128
+		),
+		"amount" => array(
+			"type"				=> "decimal",
+			"integerPart"		=> 5,
+			"fractionalPart"	=> 2,
+			"unsigned"			=> true,
+			"null"				=> false,
+			"default"			=> 0.0
+		),
+		"real_date" => array(
+			"type"		=> "date",
+			"null"		=> true
+		),
+		"type" => array(
+			"type"		=> "string",
+			"maxlength"	=> 24,
+			"null"		=> true
+		),
+		"third_party" => array(
+			"type"		=> "string",
+			"maxlength"	=> 64,
+			"null"		=> true
+		),
+		"short_label" => array(
+			"type"		=> "string",
+			"maxlength"	=> 128,
+			"null"		=> true
+		),
+	);
+	
+	protected static $keys = array(
+		"primary" => array( "transaction_id" ),
+		"foreign" => array(
+			array(
+				"fields" => "account_id",
+				"table" => "account",
+				"references" => "account_id",
+				"onDelete" => "cascade",
+				"onUpdate" => "cascade"
+			),
+		)
+	);
+
+	public $transaction_id;
+	public $account_id;
+	public $transaction_date;
+	public $value_date;
+	public $label;
+	public $amount;
+	public $real_date;
+	public $type;
+	public $third_party;
+	public $short_label;
+}
+
+?>
+
