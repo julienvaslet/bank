bfb2448a820bfc3f313346165c17258265066036
Julien Vaslet <julien.vaslet@gmail.com>
2014-03-26 14:04:34 +0100

    Update common.php

--- common.php
+++ common.php
@@ -10,6 +10,69 @@ use database\Object;
 
 new Database( "127.0.0.1", 3306, "money", "money", "money" );
 
+
+/*
+ * Global functions
+ */
+ 
+function parseTransactionLabel( $label, $transactionDate )
+{
+	$parsedLabel = array(
+		"real_date" => NULL,
+		"type" => NULL,
+		"third_party" => NULL,
+		"short_label" => NULL
+	);
+	
+	$transactionDateElements = explode( "-", $transationDate );
+	
+	// Credit card
+	if( preg_match( "/^PAIEMENT CB ([0-9]{2})([0-9]{2}) /", $label, $matches ) )
+	{
+		$parsedLabel["type"] = "credit-card";
+		$pasredLabel["real_date"] = $transactionDateElements[0]."-".$matches[2]."-".$matches[1];
+		$parsedLabel["short_label"] = preg_replace( "/ CARTE [\*0-9]+$/", "", substr( $label, strlen( $matches[0] ) ) );
+	}
+	
+	// Check
+	else if( preg_match( "/^CHEQUE /", $label, $matches ) )
+	{
+		$parsedLabel["type"] = "check";
+		$parsedLabel["short_label"] = substr( $label, strlen( $matches[0] ) );
+	}
+	
+	// Transfer
+	else if( preg_match( "/^VIR (SEPA)? /", $label, $matches ) )
+	{
+		$parsedLabel["type"] = "transfer";
+		$parsedLabel["short_label"] = substr( $label, strlen( $matches[0] ) );
+	}
+	
+	// Contribution
+	else if( preg_match( "/^F COTIS /", $label, $matches ) )
+	{
+		$parsedLabel["type"] = "contribution";
+		$parsedLabel["short_label"] = substr( $label, strlen( $matches[0] ) );
+	}
+	
+	// Debit
+	else if( preg_match( "/^PRLV (SEPA)? /", $label, $matches ) )
+	{
+		$parsedLabel["type"] = "debit";
+		$parsedLabel["short_label"] = substr( $label, strlen( $matches[0] ) );
+	}
+	
+	// Withdrawal
+	else if( preg_match( "/^RETRAIT DAB ([0-9]{2})([0-9]{2}) /", $label, $matches ) )
+	{
+		$parsedLabel["type"] = "withdrawal";
+		$pasredLabel["real_date"] = $transactionDateElements[0]."-".$matches[2]."-".$matches[1];
+		$parsedLabel["short_label"] = preg_replace( "/ CARTE [\*0-9]+$/", "", substr( $label, strlen( $matches[0] ) ) );
+	}
+	
+	return $parsedLabel;
+}
+
 /*require_once( RootPath.'/classes/internet/Template.class.php' );
 $template = new Template( RootPath. '/templates/'. TemplateName );
 
