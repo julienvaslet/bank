36b6956f948975ab2737d88781d94086ab20dbcc
Julien Vaslet <julien.vaslet@gmail.com>
2014-03-26 13:25:42 +0100

    Update import.php

--- import.php
+++ import.php
@@ -90,16 +90,18 @@ if( array_key_exists( "account", $_POST ) && !empty( $_POST["account"] ) )
 				}
 
 				// Process CSV line
+				$parsedLabel = parseTransactionLabel( $values[3] );
+				
 				$transactionId = Transaction::create( array(
 					"account_id" => $account->account_id,
 					"transaction_date" => convertBankDate( $values[0] ),
 					"value_date" => convertBankDate( $values[1] ),
 					"amount" => floatval( $values[2] ),
 					"label" => $values[3],
-					"real_date" => NULL,
-					"type" => NULL,
-					"third_party" => NULL,
-					"short_label" => NULL
+					"real_date" => $parsedLabel["real_date"],
+					"type" => $parsedLabel["type"],
+					"third_party" => $parsedLabel["third_party"],
+					"short_label" => $parsedLabel["short_label"]
 				) );
 
 				if( $transactionId !== false )
@@ -110,23 +112,10 @@ if( array_key_exists( "account", $_POST ) && !empty( $_POST["account"] ) )
 			}
 			else
 			{
-				// handle error
+				// handle error ?
 			}
 		}
 	}
-
-/* Parse CSV:
-date inverted... 
-Date d'opération,Date de valeur,Montant,Libellé,Solde
-01/27/2014,01/27/2014,-14.53,PAIEMENT CB 2501 TOULOUSE CARREFOUR CITY CARTE 05973845,837.55
-01/28/2014,01/28/2014,-45.00,CHEQUE 3910275,748.74
-01/28/2014,01/28/2014,2026.86,VIR ATOS INFOGERANCE ATOS INFOGERANCE,2775.60
-02/04/2014,02/04/2014,-950.00,VIR SEPA LOYER ESTEBE 2014-02,1756.29
-02/08/2014,02/08/2014,-20.00,RETRAIT DAB 0802 REF02220A00 CARTE **3845,705.49
-02/10/2014,02/10/2014,-40.34,PRLV SEPA FREE TELECOM FREE HAUTDEBIT 466540192 FHD 466540192,665.15
-02/12/2014,02/12/2014,237.50,VIR PETIT REMI LOYER FEVRIER,856.09
-02/12/2014,02/01/2014,-4.16,F COTIS EUROCOMPTE JEUNE,1098.09
- */
 }
 
 function convertBankDate( $bank_date )
